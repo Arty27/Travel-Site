20472f0d8634154e10f19fca29851ec0c4b1d448
NV <Vinay.N@tyson.com>
2021-09-04 16:39:30 +0530

    home page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Pages/Home';
 function App() {
   return (
     <div className="App">
      <Router>
         <Navbar/>
         <Switch>
-          <Route path='/' exact></Route>
+          <Route path='/' exact component={Home}></Route>
         </Switch>
      </Router>
     </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from './button';
-import './navbar.css';
+import { Button } from './Button';
+import './Navbar.css';
 function Navbar() {
     const [click,setClick]=useState(false)
     const handleClick=()=>setClick(!click)
