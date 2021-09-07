369864708226a39621d0f5583ad74a1a78bf798d
NV <Vinay.N@tyson.com>
2021-09-07 13:01:33 +0530

    footer added

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import Card from '../Card';
+import Footer from '../Footer';
 import Hero from '../Hero';
 function Home() {
     return (
         <>
             <Hero/>
             <Card/>
+            <Footer/>
         </>
     )
 }
