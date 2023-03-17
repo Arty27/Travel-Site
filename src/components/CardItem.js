4b2aad902da47bd391ad2661cd34c337eb986b42
N <Vinay.N@tyson.com>
2023-03-17 18:42:16 +0530

    firebase deploy added and image issue fixed

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,7 +6,7 @@ function CardItem(props) {
             <li className="cards__item">
                 <Link className="cards__item__link" to={props.path}>
                     <figure className="cards__item__pic-wrap" data-category={props.label}>
-                        <img src={require(`../images/${props.src}`).default} alt={props.src} className="cards__item__img"/>
+                        <img src={require(`../images/${props.src}`)} alt={props.src} className="cards__item__img"/>
                     </figure>
                     <div className="cards__item__info">
                         <h5 className="cards__item__text">{props.text}</h5>
